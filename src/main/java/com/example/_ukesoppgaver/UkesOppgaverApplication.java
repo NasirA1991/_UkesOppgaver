2cba62e77e46544df5c73c9a9dc3fba73bfc39c4
nasir91 <evilleader91@gmail.com>
2023-02-08 00:28:39 +0100

    Initial commit

---
+++ src/main/java/com/example/_ukesoppgaver/UkesOppgaverApplication.java
@@ -0,0 +1,13 @@
+package com.example._ukesoppgaver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UkesOppgaverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UkesOppgaverApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/_ukesoppgaver/UkesOppgaverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example._ukesoppgaver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UkesOppgaverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
